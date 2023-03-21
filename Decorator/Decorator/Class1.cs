4502ddefd18eda94767218f4c2e33d2bf219d976
bogdanborzianytsia <128235605+bogdanborzianytsia@users.noreply.github.com>
2023-03-21 18:38:20 +0200

    Add files via upload

---
+++ Decorator/Decorator/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    abstract class Tree
+    {
+        public string Name { get; protected set; }
+        public string photo { get; protected set; }
+        public Tree(string n, string photo)
+        {
+            this.Name = n;
+            this.photo = photo;
+        }
+        public abstract int GetYear();
+    }
+
+    class YoungTree : Tree
+    {
+        public YoungTree() : base("Tree1", "Tree.jfif")
+        { }
+        public override int GetYear()
+        {
+            return 10;
+        }
+    }
+    class OldTree : Tree
+    {
+        public OldTree()
+            : base("Tree2", "Tree2.jfif")
+        { }
+        public override int GetYear()
+        {
+            return 100;
+        }
+    }
+
+    abstract class TreeDecorator : Tree
+    {
+        protected Tree tree;
+        public TreeDecorator(string n, string g, Tree boats) : base(n, g)
+        {
+            this.tree = boats;
+        }
+    }
+
+    class YoungerTree : TreeDecorator
+    {
+        public YoungerTree(Tree p)
+            : base(p.Name + ", Young", p.photo, p)
+        { }
+
+        public override int GetYear()
+        {
+            return tree.GetYear();
+        }
+    }
+
+    class OlderTree : TreeDecorator
+    {
+        public OlderTree(Tree p)
+            : base(p.Name + ",Old", p.photo, p)
+        { }
+
+        public override int GetYear()
+        {
+            return tree.GetYear() + 400;
+        }
+    }
+}
---
+++ Decorator/Decorator/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decorator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = null;
+            Tree boats1 = new YoungTree();
+            boats1 = new YoungerTree(boats1);
+            richTextBox1.Text += ("Название:", boats1.Name);
+            richTextBox1.Text += ("Возраст:", boats1.GetYear()) + "\n";
+            pictureBox1.ImageLocation = boats1.photo;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = null;
+            Tree boats2 = new YoungTree();
+            boats2 = new OlderTree(boats2);
+            richTextBox1.Text += ("Название:", boats2.Name);
+            richTextBox1.Text += ("Возраст:", boats2.GetYear()) + "\n";
+            pictureBox1.ImageLocation = boats2.photo;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = null;
+            Tree boats3 = new OldTree();
+            boats3 = new YoungerTree(boats3);
+            boats3 = new OlderTree(boats3);
+            richTextBox1.Text += ("Название:", boats3.Name);
+            richTextBox1.Text += ("Возраст:", boats3.GetYear()) + "\n";
+            pictureBox1.ImageLocation = boats3.photo;
+        }
+    }
+}
